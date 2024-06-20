5997a6f125eea187f281ea44af7c7f2a95d6e18f
noraphit <noraphit.p@gmail.com>
2024-06-20 13:28:19 +0700

    Import and Add MUI App bar

--- cb-data-dashboard/src/components/Login/Login.tsx
+++ cb-data-dashboard/src/components/Login/Login.tsx
@@ -3,12 +3,12 @@ import { signIn, signOut, useSession } from "next-auth/react";
 
 const Login = () => {
   const { data: session } = useSession();
+  const userProfileImg = session?.user?.image as string;
 
   if (session) {
     return <>
       Signed in as {session?.user?.email} <br />
       <p>Welcome {session?.user?.name}</p>
-      <Avatar alt={session?.user?.name} src={session?.user?.image} />
       <button onClick={() => signOut()}>Sign Out</button>
     </>
   }
