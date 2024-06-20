e52c805e27ceeaba69aab1796307f1e4f4b1b46b
noraphit <noraphit.p@gmail.com>
2024-06-20 14:36:39 +0700

    Add module CSS + SCSS

--- cb-data-dashboard/src/pages/index.tsx
+++ cb-data-dashboard/src/pages/index.tsx
@@ -4,20 +4,22 @@ import SideMenu from "../components/SideMenu";
 import { useSession } from "next-auth/react";
 import Head from "next/head";
 import Login from "@/components/Login";
+import scss from "./Home.module.scss";
+import React from "react";
 
-export default function Home() {
+const Home: React.FC = () => {
   const { data: session } = useSession();
 
   return (
     <>
       <Head>
-        <title>Data Dashboard</title>
+        <title>DataSoft - Dashboard</title>
         <meta name="description" content="Data Dashboard" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
+      <main className={scss.main}>
         <Header />
         <SideMenu />
         <Dashboard />
@@ -26,3 +28,5 @@ export default function Home() {
     </>
   );
 };
+
+export default Home;
